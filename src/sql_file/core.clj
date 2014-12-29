b1f4441d9cc821a043dd36dfc99a5dd4ad845e96
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-12-29 10:43:10 -0500

    Add license declarations at the top of all source files.

--- src/sql_file/core.clj
+++ src/sql_file/core.clj
@@ -1,3 +1,15 @@
+; Copyright (c) KSM Technology Partners.
+; All rights reserved.
+;
+; The use and distribution terms for this software are covered by the
+; Apache License, version 2.0 (http://opensource.org/licenses/Apache-2.0)
+; which can be found in the file LICENSE at the root of this distribution.
+;
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+;
+; You must not remove this notice, or any other, from this software.
+
 (ns sql-file.core
   (:use sql-file.util)
   (:require [clojure.tools.logging :as log]
