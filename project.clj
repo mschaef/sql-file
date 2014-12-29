43a0bacaf9f414081a60b81790b89400525789bc
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-12-29 11:16:10 -0500

    Correct license declarations to match the Apache License 2.0 standard form.

--- project.clj
+++ project.clj
@@ -1,12 +1,19 @@
-; Copyright (c) KSM Technology Partners.
-; All rights reserved.
+; Copyright (c) 2014 KSM Technology Partners
 ;
-; The use and distribution terms for this software are covered by the
-; Apache License, version 2.0 (http://opensource.org/licenses/Apache-2.0)
-; which can be found in the file LICENSE at the root of this distribution.
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
 ;
-; By using this software in any fashion, you are agreeing to be bound by
-; the terms of this license.
+;       http://www.apache.org/licenses/LICENSE-2.0
+;
+; The license is also includes at the root of the project in the file
+; LICENSE.
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
 ;
 ; You must not remove this notice, or any other, from this software.
 
--- resources/schema-sql-file-0.sql
+++ resources/schema-sql-file-0.sql
@@ -1,20 +1,21 @@
--- Copyright (c) KSM Technology Partners.
--- All rights reserved.
--- 
--- The use and distribution terms for this software are covered by the
--- Apache License, version 2.0 (http://opensource.org/licenses/Apache-2.0)
--- which can be found in the file LICENSE at the root of this distribution.
--- 
--- By using this software in any fashion, you are agreeing to be bound by
--- the terms of this license.
--- 
--- You must not remove this notice, or any other, from this software.
-
-
--- schema-sql-file-0.sql
+-- Copyright (c) 2014 KSM Technology Partners
 --
--- Base sql-file schema. This contains administration information used
--- by sql-file to maintain the schemas stored within a database file.
+-- Licensed under the Apache License, Version 2.0 (the "License")--
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+-- The license is also includes at the root of the project in the file
+-- LICENSE.
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- You must not remove this notice, or any other, from this software.
 
 CREATE CACHED TABLE sql_file_schema (
   schema_name VARCHAR(32) NOT NULL PRIMARY KEY,
