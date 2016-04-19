1b09e43ee17742a472f881783599169c0b40d6ba
Mike Schaeffer <mike@mschaef.com>
2016-04-19 19:40:28 -0400

    Update licensing and artifact name.

--- project.clj
+++ project.clj
@@ -1,35 +1,39 @@
-; Copyright (c) 2014 KSM Technology Partners
-;
-; Licensed under the Apache License, Version 2.0 (the "License");
-; you may not use this file except in compliance with the License.
-; You may obtain a copy of the License at
-;
-;       http://www.apache.org/licenses/LICENSE-2.0
-;
-; The license is also includes at the root of the project in the file
-; LICENSE.
-;
-; Unless required by applicable law or agreed to in writing, software
-; distributed under the License is distributed on an "AS IS" BASIS,
-; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-; See the License for the specific language governing permissions and
-; limitations under the License.
-;
-; You must not remove this notice, or any other, from this software.
+;; Copyright (c) 2015-2016 Michael Schaeffer
+;;
+;; Licensed as below.
+;;
+;; Portions Copyright (c) 2014 KSM Technology Partners
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;       http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; The license is also includes at the root of the project in the file
+;; LICENSE.
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+;;
+;; You must not remove this notice, or any other, from this software.
 
-(defproject com.ksmpartners/sql-file "0.1.0"
+(defproject sql-file "0.1.1"
 
   :description "Library for managing data files used by embedded databases."
 
-  :url "http://www.ksmpartners.com/"
+  :url "http://www.mschaef.com/"
   :license {:name "The Apache Software License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :scm {:name "git"
-        :url "https://github.com/ksmpartners/sql-file.git"}
+        :url "https://github.com/mschaef/sql-file.git"}
 
   :main sql-file.main
-  :aot [sqi-file.main]
+  :aot [sql-file.main]
 
   :jvm-opts ["-Djava.util.logging.config.file=logging.properties" ]
 
