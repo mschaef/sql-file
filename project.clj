e0446f2034bb86af46b8f409e7c05c5a105e16ed
Mike Schaeffer <mike@mschaef.com>
2016-05-05 14:51:59 -0400

    Update to version 0.2.0-pre

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
 ;;
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject sql-file "0.1.1"
+(defproject sql-file "0.2.0-pre0"
 
   :description "Library for managing data files used by embedded databases."
 
