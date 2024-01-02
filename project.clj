b5c023ffba1e27c079007116d710b8fb5f8a6a04
Mike Schaeffer <mike@mschaef.com>
2024-01-02 15:44:53 -0500

    Test the build as part of the release process.

--- project.clj
+++ project.clj
@@ -52,6 +52,7 @@
                                      :sign-releases false}]]
 
   :release-tasks [["vcs" "assert-committed"]
+                  ["test"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag" "--no-sign"]
