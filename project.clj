4dbc34fa372852188fae29d20abe065985414884
Mike Schaeffer <mike@mschaef.com>
2025-05-17 14:10:28 -0400

    Version 0.4.13

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
 ;;
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject com.mschaef/sql-file "0.4.13-SNAPSHOT"
+(defproject com.mschaef/sql-file "0.4.13"
 
   :description "One stop shopping for embedding an HSQLDB database and managing schemas for same."
 
